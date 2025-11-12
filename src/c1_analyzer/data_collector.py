be35b7273069d2779d6230fc22bce77887a1edb3
anastasiya <karinkagubarmeow@mail.ru>
2025-11-12 19:04:58 +0800

    Add working data collector

--- src/c1_analyzer/data_collector.py
+++ src/c1_analyzer/data_collector.py
@@ -1,2 +1,7 @@
-print("C1 Vocabulary Data Collector")
-print("This will generate vocabulary data")
+def generate_data():
+    words = ["advanced", "vocabulary", "analysis", "project"]
+    print("C1 Level Words:", words)
+    return words
+
+if __name__ == "__main__":
+    generate_data()
